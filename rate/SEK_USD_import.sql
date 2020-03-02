63cf3dd7dbf493bf5a6d58b61cd5d4b6821bae0c
OfAllTrades-jack <61686426+OfAllTrades-jack@users.noreply.github.com>
2020-03-02 12:36:37 +0100

    Create SEK_USD_import.sql

---
+++ rate/SEK_USD_import.sql
@@ -0,0 +1,18 @@
+DROP TABLE IF EXISTS sekusd;
+
+CREATE TABLE `sekusd` (
+  `date` date NOT NULL,
+  `price` decimal(20,8) NOT NULL,
+  PRIMARY KEY (`date`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+LOAD DATA INFILE '/var/lib/mysql-files/SEK_USD_RIKSBANKEN.csv'
+INTO TABLE sekusd
+FIELDS TERMINATED BY ';'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(@date,@info1,@info2,@price)
+SET date = STR_TO_DATE(@date, '%Y-%m-%d'),
+price=@price;
